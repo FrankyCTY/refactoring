2b5cdeffb6d70a9d927c1a6fb0bcdf0f2201704b
Tak Yu Chan <frankychantakyu@yahoo.com.hk>
2022-07-16 18:14:00 +1200

    refactor(renderHtml): add renderHtml to show how easy it is to add new use case

--- refactor03/refactor03.js
+++ refactor03/refactor03.js
@@ -9,6 +9,7 @@ function usd(aNumber) {
 }
 
 function statement(invoice, plays) {
+  console.log(renderHtml(createStatementData(invoice, plays)));
   return renderPlainText(createStatementData(invoice, plays));
 }
 
@@ -27,5 +28,21 @@ function renderPlainText(data) {
   // Append to result string
   result += `Amount owed is ${usd(data.get('totalAmount'))}\n`;
   result += `You earned ${data.get('totalVolumeCredits')} credits\n`;
+  return result;
+}
+
+function renderHtml(data) {
+  let result = `<h1>Statement for ${data.get('customer')}</h1>\n`;
+  result += '<table>\n';
+  result += '<tr><th>play</th><th>seats</th><th>cost</th></tr>';
+  for (let perf of data.get('performances')) {
+    result += ` <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
+  }
+  result += '</table>\n';
+  result += `<p>Amount owed is <em>${usd(data.get('totalAmount'))}</em></p>\n`;
+  result += `<p>You earned <em>${data.get(
+    'totalVolumeCredits'
+  )}</em> credits</p>\n`;
   return result;
 }
