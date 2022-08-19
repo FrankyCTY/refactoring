2ca726d771fa11231e5b683a5f159280169f3fc0
Tak Yu Chan <frankychantakyu@yahoo.com.hk>
2022-08-19 22:26:41 +1200

    feat/replace-subclass-with-delegate

---
+++ replaceSubclassWithDelegate/refactor.js
@@ -0,0 +1,69 @@
+class Booking {
+  constructor(show, date) {
+    this._show = show;
+    this._date = date;
+  }
+
+  get hasTalkback() {
+    return this._premiumDelegate
+      ? this._premiumDelegate.hasTalkback
+      : this._show.hasOwnProperty('talkback') && !this.isPeakDay;
+  }
+
+  get basePrice() {
+    return this._premiumDelegate
+      ? this._premiumDelegate.basePrice
+      : this._privateBasePrice;
+  }
+
+  get _privateBasePrice() {
+    let result = this._show.price;
+    if (this.isPeakDay) result += Math.round(result * 0.15);
+    return result;
+  }
+
+  get hasDinner() {
+    return this._premiumDelegate ? this._premiumDelegate.hasDinner : undefined;
+  }
+
+  _bePremium(extras) {
+    this._premiumDelegate = new PremiumBookingDelegate(this, extras);
+  }
+}
+
+// Expanded / modified behavior on "superclass" - Upgrade "superclass" with different / extra behaviors with composition & 2 way referencing
+class PremiumBookingDelegate {
+  constructor(hostBooking, extras) {
+    this._host = hostBooking;
+    this._extras = extras;
+  }
+
+  get hasTalkback() {
+    return this._host._show.hasOwnProperty('talkback');
+  }
+
+  get basePrice() {
+    return Math.round(this._host._privateBasePrice + this._extras.premiumFee);
+  }
+
+  get hasDinner() {
+    return this._extras.hasOwnProperty('dinner') && !this._host.isPeakDay;
+  }
+}
+
+// Use factory function as indirection - Reduce breakage happens after removing subclasses
+function createBooking(show, date) {
+  return new Booking(show, date);
+}
+
+function createPremiumBooking(show, date, extras) {
+  const result = new Booking(show, date, extras);
+  result._bePremium(extras);
+  return result;
+}
+
+// booking client
+aBooking = createBooking(show, date);
+
+// premium client
+aBooking = createPremiumBooking(show, date, extras);
